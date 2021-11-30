b246fcafa4813441248422bf017ecc177b1b7f72
"Luke Newman" <"newmanlu@tcd.ie">
2021-11-30 10:45:42 +0100

    slot start

--- slotGame.sol
+++ slotGame.sol
@@ -9,4 +9,21 @@ contract mortal {
 
 contract slotGame is mortal{
 
+    
+
+    function randomGenerate(uint mod) internal view returns(uint){
+    
+            uint randNo = uint(keccak256(abi.encodePacked(
+            now,
+            block.difficulty,
+            msg.sender)
+            ))%mod;
+
+        return randNo;
+    }
+
+    function value() external view returns(uint){
+        return randomGenerate(10);
+    }
+
 }
