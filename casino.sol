ab3d28cfd8d32ffe6ed84dfafebd5210074f9690
"Florian Hartl" <"florian@hartl-home.eu">
2021-12-01 16:36:16 +0100

    commenting

--- casino.sol
+++ casino.sol
@@ -7,6 +7,7 @@ contract mortal {
     function kill() public { if (msg.sender == owner) selfdestruct(owner); }
 }
 
+
 contract casino is mortal{
     struct tokenList{
         address userId;     //WalletID of the user. The first wallet is always the casino owner (It is initialized when deploying the contract)
@@ -303,9 +304,10 @@ contract casino is mortal{
 
 // ---------------------Blackjack Functions--------------------------------------
 
-    string[] possibleCards = ["2","3","4","5","6","7","8","9","Jack","Queen", "King", "Ace"];
-    uint[] possibleValues =  [ 2,  3,  4,  5,  6,  7,  8,  9,  10,    10,      10,     11  ];
+    string[] possibleCards = ["2","3","4","5","6","7","8","9","10", "Jack","Queen", "King", "Ace"];
+    uint[] possibleValues =  [ 2,  3,  4,  5,  6,  7,  8,  9,  10,   10,     10,     10,     11];
 
+    //A BlackJack game consists of the player ID, the cards the dealer has on his table, the gamber's cards and the stakte the two parties play for
     struct blackJackGame{
         address playerID;
         uint256[] dealerCards;
@@ -313,22 +315,27 @@ contract casino is mortal{
         uint256 stake;
     }
     
+    //Initialize the blackjack game table, where all the games will be stored
+    blackJackGame[] blackJackGames; 
 
-    blackJackGame[] blackJackGames;// = new blackJackGame[](10);
-
+    //returns the length of the game table (how many games were started so far)
     function getNumberBlackjackGames() private view returns (uint256){
         return blackJackGames.length;
     }
 
+    //One player can only play one game at a time. We need to check whether another game is still running
     function noGameRunning(address playerID) private view returns (bool){
         for (uint256 i = 0; i < getNumberBlackjackGames(); i++){     //Determine if there is a game by this player
             if(blackJackGames[i].playerID == playerID){
-                return false;
+                return false;   //If a game by this player is found "false" is returned
             }
         }
-        return true;
+        return true;            //No game was found, so we can return true
     }
+
+    //This function determines on which position of the game array the currently played game is
     function getGameIndex(address playerID) private view returns (uint256){
+        require(noGameRunning(playerID) == false);      //If there is no game, no index can be returned
         uint256 gameIndex = 0;
         for (uint256 i = 0; i < getNumberBlackjackGames(); i++){     
             if(blackJackGames[i].playerID == playerID){
@@ -336,9 +343,9 @@ contract casino is mortal{
                 return gameIndex;
             }
         }
-        return gameIndex;
     }
 
+    /*
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
         if (_i == 0) {
             return "0";
@@ -360,18 +367,23 @@ contract casino is mortal{
         }
         return string(bstr);
     }
+    */
 
+    //This function returns a random number between 0 and 12 which represents our cards
     function getRandomCard() private view returns (uint256){
-        uint cardResult = randomGenerate(11);
+        uint cardResult = randomGenerate(12);
         return cardResult;
     }
-    function getNameOfCard(uint256 cardValue) private view returns (string memory){
-        return possibleCards[cardValue];
+
+    //This function takes the index of the card and returns its name. It is NOT used right now
+    function getNameOfCard(uint256 cardIndex) private view returns (string memory){
+        return possibleCards[cardIndex];
     }
 
+    //This function takes the playerID and returns its wallet ID
     function getIndexOfGambler(address playerID) private view returns (uint256){
         uint256 index;          
-        uint256 numCustomers = getNumberCustomers();                        //The users index in the casino's tokenLists
+        uint256 numCustomers = getNumberCustomers();                        
         for (uint256 i = 0; i < numCustomers; i++){                         //Determine the index of the gambler in the tokenLists
             if(tokenLists[i].userId == playerID){
                 index = i;
@@ -380,16 +392,18 @@ contract casino is mortal{
         return index;
     }
 
+    //This function is the start to a blackjack game. It distributes the first cards.
+    //We simplified blackjack a bit: Every card is there infinite times, and an Ace is always worth 11.
     function StartGameBlackjack (uint256 stake) public{
-        address playerID = msg.sender; 
-        require(enoughMoneyForBid(stake, stake, playerID));
-        tokenLists[0].amount -= stake; //Substract stake, it is in the game newTokenList
-        uint256 playerTokenID = getIndexOfGambler(msg.sender);
-        tokenLists[playerTokenID].amount -= stake;
-        uint256[] memory dealerCardsNew = new uint256[](1);
-        uint256[] memory gamblerCardsNew = new uint256[](2);
+        address playerID = msg.sender;                      //The player is the one who started the game
+        require(enoughMoneyForBid(stake, stake, playerID)); //Every party has to have enough tokens to perform the results of the game
+        tokenLists[0].amount -= stake;                      //Substract stake from the casino's accounts, it is now in the game 
+        uint256 playerTokenID = getIndexOfGambler(msg.sender);//Get the index of the gambler in the tokenLists
+        tokenLists[playerTokenID].amount -= stake;          //Substract the stake from the gambler's account
+        uint256[] memory dealerCardsNew = new uint256[](1); //At first the dealer gets one card
+        uint256[] memory gamblerCardsNew = new uint256[](2);//The gambler gets two cards
         
-        require(noGameRunning(playerID), "You already play a game");
+        require(noGameRunning(playerID), "You already play a game");//Anyone can only play one game at a time
         uint256 playerIndex;                                    //The users index in the casino's tokenLists
         for (uint256 i = 0; i < getNumberCustomers(); i++){     //Determine the index of the gambler in the tokenLists
             if(tokenLists[i].userId == playerID){
@@ -397,16 +411,16 @@ contract casino is mortal{
             }
         }
             
-        //draw first card
-        gamblerCardsNew[0] = getRandomCard();
-        //draw second card
-        gamblerCardsNew[0] = getRandomCard();
-        //draw dealer card
-        dealerCardsNew[0] = getRandomCard();
+        gamblerCardsNew[0] = getRandomCard(); //draw first card
+        gamblerCardsNew[0] = getRandomCard(); //draw second card
+        dealerCardsNew[0] = getRandomCard();  //draw dealer's card
 
+        //Create a new game entry and push it to the Games-List. After that it's the player's choice to draw a card or stand.
         blackJackGame memory game = blackJackGame(msg.sender, dealerCardsNew, gamblerCardsNew, stake);
         blackJackGames.push(game);
     }
+
+    //This function deletes a game (after it finished)
     function deleteGame(uint256 gameIndex) private{
         //blackJackGames[gameIndex] = blackJackGames[blackJackGames.length - 1];
         //delete blackJackGames[blackJackGames.length - 1];
@@ -414,17 +428,20 @@ contract casino is mortal{
        delete blackJackGames[gameIndex];
     }
 
+    //If the gambler loses, the casino gets the money from the bet and the game is deleted
     function lost(uint256 gameIndex, uint256 stake) private{
         tokenLists[0].amount += 2*stake;
         deleteGame(gameIndex);
     }
 
+    //If the gambler wins, they get the money from the bet and the game is deleted
     function win(uint256 gameIndex, address gamblerAddress, uint256 stake) private{
         uint256 index = getIndexOfGambler(gamblerAddress);
         tokenLists[index].amount += 2*stake;
         deleteGame(gameIndex);
     }
 
+    //If there is a tie, the casino and the gamber get their stake back and the game is deleted 
     function tie(uint256 gameIndex, address gamblerAddress, uint256 stake) private{
         uint256 index = getIndexOfGambler(gamblerAddress);
         tokenLists[0].amount += stake;
@@ -432,14 +449,17 @@ contract casino is mortal{
         deleteGame(gameIndex);
     }
 
+    //This function can be called by the gambler to draw one more card.
     function draw1Card() public{
+        require(noGameRunning(msg.sender) == false); 
         uint256 gameIndex = getGameIndex(msg.sender);
         uint256 newCardValue = getRandomCard();
-        blackJackGames[gameIndex].gamblerCards.push(newCardValue);
-        if(calculateValueOfHand(gameIndex, true) == 21){win(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}
-        if(calculateValueOfHand(gameIndex, true) > 21){lost(gameIndex, blackJackGames[gameIndex].stake);}
+        blackJackGames[gameIndex].gamblerCards.push(newCardValue);//Add the new card to the game
+        if(calculateValueOfHand(gameIndex, true) == 21){win(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}//if they reached 21, they directly win
+        if(calculateValueOfHand(gameIndex, true) > 21){lost(gameIndex, blackJackGames[gameIndex].stake);}//If they have more than 21, they directly lose
     }
 
+    //This function calculates the value of a hand for gambler or dealer for a given game index
     function calculateValueOfHand(uint256 gameIndex, bool gambler) private view returns (uint256){
         uint256[] memory valuesIndizes;
         if(gambler){
@@ -450,17 +470,21 @@ contract casino is mortal{
         }
         uint256 entireValue = 0;
         for (uint256 i=0; i<valuesIndizes.length; i++){
-            entireValue += possibleValues[valuesIndizes[i]];
+            entireValue += possibleValues[valuesIndizes[i]];    //the cards have specified values that come from the table above
         }
         return entireValue;
     }
+
+    //This function returns the current game status of the gambler including both their value and the one of the dealer
     function calculateBothHands() public view returns(uint256[2] memory){
         uint256 gameIndex = getGameIndex(msg.sender);
         uint256[2] memory returnValues;
-        returnValues[0] = calculateValueOfHand(gameIndex, true);
-        returnValues[1] = calculateValueOfHand(gameIndex, false);
+        returnValues[0] = calculateValueOfHand(gameIndex, true); //The value of the gambler's cards
+        returnValues[1] = calculateValueOfHand(gameIndex, false);//The value of the dealer's card(s)
         return (returnValues);
     }
+
+    //This function returns the current game status of any stated gambler including both their value and the one of the dealer
     function calculateBothHands(uint256 gameIndex) private view returns(uint256[2] memory){
         uint256[2] memory returnValues;
         returnValues[0] = calculateValueOfHand(gameIndex, true);
@@ -468,7 +492,7 @@ contract casino is mortal{
         return (returnValues);
     }
 
-    //Only for devellopping
+    //Only for devellopping: All Handy can be shown
     function getAllHands() private view returns(uint[] memory, uint[] memory){
         uint256[] memory handsGamblers = new uint256[](blackJackGames.length);
         uint256[] memory handsDealer = new uint256[](blackJackGames.length);
@@ -480,23 +504,25 @@ contract casino is mortal{
         return (handsGamblers, handsDealer);
     }
 
+    //This function adds one card to the dealer's repertoire
     function draw1CardDealer(uint256 gameIndex) private{
         uint256 newCardValue = getRandomCard();
         blackJackGames[gameIndex].dealerCards.push(newCardValue); 
     }
 
+    //This function can be called by the gambler to not draw any more cards. The game is finished from here.
     function stand() public{
         uint256 gameIndex = getGameIndex(msg.sender);
-        while(calculateValueOfHand(gameIndex, false)<17){
+        while(calculateValueOfHand(gameIndex, false)<17){       //If the dealer has cards worth less than 17 they have to draw more card(s)
             draw1CardDealer(gameIndex);
         }
-        uint256 valueGambler = calculateValueOfHand(gameIndex, true);
-        uint256 valueDealer = calculateValueOfHand(gameIndex, false);
-        if(valueDealer > 21){win(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}
+        uint256 valueGambler = calculateValueOfHand(gameIndex, true);       //The value of the gambler's cards is calculated
+        uint256 valueDealer = calculateValueOfHand(gameIndex, false);       //The value of the dealer's cards is calculated
+        if(valueDealer > 21){win(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}                  //If the dealer has more than 21, the gambler wins
         else{
-            if(valueGambler > valueDealer){win(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}
-            if(valueGambler < valueDealer){lost(gameIndex, blackJackGames[gameIndex].stake);}
-            if(valueGambler < valueDealer){tie(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}
+            if(valueGambler > valueDealer){win(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}    //If the gambler's cards's worth is higher, the gambler wins
+            if(valueGambler < valueDealer){lost(gameIndex, blackJackGames[gameIndex].stake);}               //If the dealers's cards's worth is higher, the gambler loses
+            if(valueGambler < valueDealer){tie(gameIndex, msg.sender, blackJackGames[gameIndex].stake);}    //If the gambler's cards's worth is the same as the dealer's, they tie
         }
     }
 
